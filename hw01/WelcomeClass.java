ee10c1558eb0b3b4bcb2feadbe00671b9d1cc403
Andrew Schillaci <ajs317@lehigh.edu>
2014-09-02 22:25:54 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+// Andrew Schillaci
+// 2 September 2014
+// CSE 002 (Fundamentals of Programming)
+
+
+//create a class
+public class WelcomeClass {
+   
+    //create a method
+     public static void main (String [] args) {
+          
+          //print out a welcome sign and a strange patter with my lehigh username in the middle
+          //and print out a random tweet
+          
+          System.out.println("-------------");
+          System.out.println("|  Welcome  |");
+          System.out.println("-------------");
+          System.out.println("  ^  ^  ^  ^  ^  ^");
+          System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+          System.out.println("|-A--J--S--3--1--7-|"); 
+          System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+          System.out.println("  v  v  v  v  v  v ");
+          System.out.println("This August my wife and I took an eleven day, 675 mile bike tour on our tandem. We circled Lake Ontario.");
+     }
+}
+
