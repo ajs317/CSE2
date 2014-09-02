0528a4aae1d000878015f9d2da674be0bf1f1506
Andrew Schillaci <ajs317@lehigh.edu>
2014-09-02 22:32:31 +0000

    updated WelcomeClass

--- hw01/WelcomeClass.java
+++ hw01/WelcomeClass.java
@@ -9,7 +9,7 @@ public class WelcomeClass {
     //create a method
      public static void main (String [] args) {
           
-          //print out a welcome sign and a strange patter with my lehigh username in the middle
+          //print out a welcome sign and a strange pattern with my lehigh username in the middle
           //and print out a random tweet
           
           System.out.println("-------------");
